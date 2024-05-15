cffa8f58c2d41d7b731c294bf51f00986e6fda69
Josué Azevedo <josue-azevedo@hotmail.com>
2024-05-15 11:17:15 -0300

    feat: get category usecase

---
+++ application/src/main/java/org/jmal/application/category/retrivie/get/GetCategoryByIdUseCase.java
@@ -0,0 +1,7 @@
+package org.jmal.application.category.retrivie.get;
+
+import org.jmal.application.UseCase;
+
+public abstract class GetCategoryByIdUseCase
+        extends UseCase<String, CategoryOutput> {
+}
