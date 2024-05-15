7283720b22a8edfa0fd33d7ba0263b944e061320
Josué Azevedo <josue-azevedo@hotmail.com>
2024-05-15 11:01:09 -0300

    feat: delete category usecase

---
+++ application/src/main/java/org/jmal/application/category/delete/DefaultDeleteCategoryUseCase.java
@@ -0,0 +1,20 @@
+package org.jmal.application.category.delete;
+
+import org.jmal.domain.category.CategoryGateway;
+import org.jmal.domain.category.CategoryID;
+
+import java.util.Objects;
+
+public class DefaultDeleteCategoryUseCase extends DeleteCategoryUseCase {
+
+    private final CategoryGateway categoryGateway;
+
+    public DefaultDeleteCategoryUseCase(final CategoryGateway categoryGateway) {
+        this.categoryGateway = Objects.requireNonNull(categoryGateway);
+    }
+
+    @Override
+    public void execute(final String anIn) {
+        this.categoryGateway.deleteById(CategoryID.from(anIn));
+    }
+}
---
+++ application/src/main/java/org/jmal/application/category/delete/DeleteCategoryUseCase.java
@@ -0,0 +1,6 @@
+package org.jmal.application.category.delete;
+
+import org.jmal.application.UnitUseCase;
+
+public abstract class DeleteCategoryUseCase extends UnitUseCase<String> {
+}
