2c8519423817ea1b657f087e634ac2f73c3e0b4c
Josué Azevedo <josue-azevedo@hotmail.com>
2024-05-16 09:36:35 -0300

    feat: configure spring boot

---
+++ infrastructure/src/main/java/org/jmal/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package org.jmal.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("org.jmal")
+public class WebServerConfig {
+}
